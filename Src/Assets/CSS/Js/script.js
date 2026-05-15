14c14abdeb3603c1e9e7d3078b87e52bcaed2801
arthurgermano69 <artgerpin@gmail.com>
2026-05-15 19:40:35 -0300

    Feat: Criacao da funcao formulario

--- Src/Assets/CSS/Js/script.js
+++ Src/Assets/CSS/Js/script.js
@@ -9,6 +9,29 @@ function validacao(){
         let usuarioDigitado = prompt("Digite seu usuario: ")
         let senhaDigitada = prompt("Digite sua senha: ")
 
+        if (usuarioDigitado === null || senhaDigitada === null){    
+        alert("login cancelado")
+        return
+
+        }if (usuarioDigitado === "" || senhaDigitada === ""){
+        alert("Por favor preencha todos os campos!")
+
+        }else if (usuarioDigitado === usuarioCorreto && senhaDigitada === senhaCorreta){
+        alert("Login realizado com sucesso, seja Bem-vindo!")
+        acessoPermitido = true
+
+        }else {
+        alert("Usuario ou senha incorretos! Tente novamente.")
+        }
     }
+}
+
+
+function formulario(){
+
+    console.log("FORMULARIO DE AVALIACAO")
+    
+    let nome = prompt("Qual e o seu nome? ")
+
 
 }
