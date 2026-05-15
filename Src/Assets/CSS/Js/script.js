e5cabedb81c73141be074920f8f940bc2c4cd1cc
arthurgermano69 <artgerpin@gmail.com>
2026-05-15 20:04:11 -0300

    Feat: Funcao formulario terminada

--- Src/Assets/CSS/Js/script.js
+++ Src/Assets/CSS/Js/script.js
@@ -10,18 +10,18 @@ function validacao(){
         let senhaDigitada = prompt("Digite sua senha: ")
 
         if (usuarioDigitado === null || senhaDigitada === null){    
-        alert("login cancelado")
-        return
+            alert("login cancelado")
+            return
 
         }if (usuarioDigitado === "" || senhaDigitada === ""){
-        alert("Por favor preencha todos os campos!")
+            alert("Por favor preencha todos os campos!")
 
         }else if (usuarioDigitado === usuarioCorreto && senhaDigitada === senhaCorreta){
-        alert("Login realizado com sucesso, seja Bem-vindo!")
-        acessoPermitido = true
+            alert("Login realizado com sucesso, seja Bem-vindo!")
+            acessoPermitido = true
 
         }else {
-        alert("Usuario ou senha incorretos! Tente novamente.")
+            alert("Usuario ou senha incorretos! Tente novamente.")
         }
     }
 }
@@ -33,5 +33,62 @@ function formulario(){
     
     let nome = prompt("Qual e o seu nome? ")
 
+    if (nome === null || nome === ""){
+        
+        alert("Formulario cancelado!")
+        return
+    }
 
+    let nota = prompt("De 1 a 10, que nota voce da para nosso site? ")
+
+    if (nota === null){
+        
+        alert("Formulario cancelado!")
+        return
+    }
+
+    while(nota < 1 || nota > 10 || isNaN(nota)){
+        
+        alert("Apenas numeros de 1 a 10!")
+        nota = prompt("De 1 a 10, que nota voce da para nosso site? ")
+    }
+
+    let gostou = prompt ("Oque voce mais gostou no site? ")
+
+    if (gostou === null || gostou === ""){
+        
+        alert("Formulario cancelado!")
+        return
+    }
+
+    let melhoria = prompt("Oque voce melhoraria no site? ")
+
+    if (melhoria === null || melhoria === ""){
+        
+        alert("Formulario cancelado!")
+        return
+    }
+
+    let recomendaria = prompt("Voce recomendaria nosso site para alguem? (sim/nao) ")
+
+    recomendaria = recomendaria.toLowerCase()
+
+    if (recomendaria === null || recomendaria === ""){
+        
+        alert("Formulario cancelado!")
+        return
+    }
+
+    while(recomendaria != "sim" && recomendaria != "nao"){
+        
+        alert("Apenas respostas de sim ou nao!")
+        recomendaria = prompt("Voce recomendaria nosso site para alguem? (sim/nao) ")
+    }
+
+    console.log("RESPOSTAS DO FORMULARIO!")
+    console.log(`Nome: ${nome}`)
+    console.log(`Nota: ${nota}`)
+    console.log(`Oque voce mais gostou: ${gostou}`)
+    console.log(`Oque voce melhoraria: ${melhoria}`)
+    console.log(`Recomendaria: ${recomendaria}`)
 }
