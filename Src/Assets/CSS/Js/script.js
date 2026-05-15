7fa46ec586debe67fc84a87325dcfc02c986abaf
arthurgermano69 <artgerpin@gmail.com>
2026-05-15 19:31:55 -0300

    Feat: Adicionando funcao de validacao completa

--- Src/Assets/CSS/Js/script.js
+++ Src/Assets/CSS/Js/script.js
@@ -2,6 +2,13 @@ function validacao(){
     
     let usuarioCorreto = "admin"
     let senhaCorreta = "123456"
-    let acessoPermitido = false\
+    let acessoPermitido = false
+
+    while (acessoPermitido === false){
+
+        let usuarioDigitado = prompt("Digite seu usuario: ")
+        let senhaDigitada = prompt("Digite sua senha: ")
+
+    }
 
 }
